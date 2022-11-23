93d83febe81ab7d8b178baac214358044a24c42b
Ka6a4oK-k <72969414+Ka6a4oK-k@users.noreply.github.com>
2022-11-24 00:24:45 +0300

    Added some components and styles

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/sidebar.css'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <h1>Quizoff</h1>
+      <ul>
+        <li>some link</li>
+        <li>another link</li>
+        <li>one more link</li>
+        <li>a link too</li>
+      </ul>
+    </div>
+  )
+}
