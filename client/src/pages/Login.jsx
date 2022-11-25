6cbdfe1919aafe4fdba1503e9d616ea5b162e12b
Ka6a4oK-k <72969414+Ka6a4oK-k@users.noreply.github.com>
2022-11-25 13:20:43 +0300

    registration validation added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import '../styles/registration-login.css'
 
 export default function Login() {
 
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const signUp = async (e) => {
+    // try {
+    //   await axios.get('http://localhost:3000/users')
+    //   setFormData({
+    //     email: '',
+    //     password: ''
+    //   })
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  }
+
+  const inputChange = (e) => {
+    setFormData(() => ({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+    )
+  }
+
   return (
     <div className='login'>
-        <h1 className='form-title'>Login</h1>
-        <form onSubmit={(e) => {e.preventDefault()}}>
-            <input type="email" name='email' placeholder='Email...'/>
-            <input type="password" name='password' placeholder='Password...'/>
-            <input type="submit" value='Log in'/>
-        </form>
+      <h1 className='form-title'>Login</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        signUp()
+      }}>
+        <input type="email" onChange={inputChange} value={formData.email} name='email' placeholder='Email...' />
+        <input type="password" onChange={inputChange} value={formData.password} name='password' placeholder='Password...' />
+        <input type="submit" value='Log in' />
+      </form>
     </div>
   )
 }
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import '../styles/registration-login.css'
+import { useEffect } from 'react'
 
 export default function Registration() {
 
+  const USERNAME_REGX = /[a-zA-Z0-9]{4,20}$/
+  const EMAIL_REGX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  const PASSWORD_REGX = /[a-zA-Z0-9]{8,20}$/
 
+  const [formValidation, setFormValidation] = useState(false)
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  useEffect(() => {
+    setFormValidation(formData.email.match(EMAIL_REGX) &&
+      formData.username.match(USERNAME_REGX) &&
+      formData.password.match(PASSWORD_REGX))
+      console.log(formData);
+  }, [formData])
+
+  const register = async (e) => {
+    e.preventDefault()
+    try {
+      await axios.post('http://localhost:3000/users', formData)
+      setFormData({
+        username: '',
+        email: '',
+        password: ''
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const inputChange = (e) => {
+    setFormData(() => ({
+      ...formData,
+      [e.target.name]: e.target.value
+    }))
+  }
+  
 
   return (
     <div className='registration'>
-        <h1 className='form-title'>Registration</h1>
-        <form onSubmit={(e) => {e.preventDefault()}}>
-            <input type="text" name='name' placeholder='Username...'/>
-            <input type="email" name='email' placeholder='Email...'/>
-            <input type="password" name='password' placeholder='Password...'/>
-            <input type="submit" value='Sign up'/>
-        </form>
+      <h1 className='form-title'>Registration</h1>
+      <form onSubmit={(e) => { register(e) }}>
+        <input type="username" onChange={inputChange} value={formData.username} name='username' placeholder='Username...' />
+        <input type="email" onChange={inputChange} value={formData.email} name='email' placeholder='Email...' />
+        <input type="password" onChange={inputChange} value={formData.password} name='password' placeholder='Password...' />
+        <input disabled={!formValidation} type="submit" value='Sign up' />
+      </form>
     </div>
   )
 }
